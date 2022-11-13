443653f4a040cf066060d58a3a759f4096fe2dfe
william malte jensen <williamjensenbc@gmail.com>
2022-11-13 01:09:56 +0100

    Add initial rendering setup

---
+++ asexual_evo_sim/simple_render_system.h
@@ -0,0 +1,37 @@
+#ifndef PHM_SIMPLE_RENDER_SYSTEM_H
+#define PHM_SIMPLE_RENDER_SYSTEM_H
+
+#include <memory>
+#include <vector>
+
+#include "phm_pipeline.h"
+#include "phm_object.h"
+
+
+namespace phm
+{
+	class SimpleRenderSystem
+	{
+
+	public:
+		SimpleRenderSystem(Device& device, VkRenderPass renderPass);
+		~SimpleRenderSystem();
+
+		SimpleRenderSystem(const SimpleRenderSystem&) = delete;
+		SimpleRenderSystem& operator=(const SimpleRenderSystem&) = delete;
+
+		void renderObjects(VkCommandBuffer commandBuffer, const std::vector<Object>& objects);
+
+	private:
+		Device& device_;
+
+		std::unique_ptr<Pipeline> pipeline_;
+		VkPipelineLayout pipelineLayout_;
+
+		void createPipelineLayout();
+		void createPipeline(VkRenderPass renderPass);
+	};
+}
+
+#endif /* PHM_SIMPLE_RENDER_SYSTEM_H */
+
---
+++ renderer/phm_model.h
@@ -0,0 +1,51 @@
+#ifndef PHM_MODEL_H
+#define PHM_MODEL_H
+
+#include "phm_device.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace phm
+{
+	class Model
+	{
+	public:
+
+		// Public member variables
+		struct Vertex
+		{
+			glm::vec3 pos;
+			glm::vec3 col;
+
+			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+		};
+
+		// Constructors
+
+		Model(Device& device, const std::vector<Vertex>& vertices);
+		~Model();
+
+		Model(const Model&) = delete;
+		Model& operator=(const Model&) = delete;
+
+		// Public methods
+
+		void bind(VkCommandBuffer commandBuffer);
+		void draw(VkCommandBuffer commandBuffer);
+
+	private:
+		Device& device_;
+		VkBuffer vertexBuffer_;
+		VkDeviceMemory vertexBufferMemory_;
+		uint32_t vertexCount_;
+
+		void createVertexBuffers(const std::vector<Vertex>& vertices);
+	};
+}
+
+#endif /* PHM_MODEL_H */
